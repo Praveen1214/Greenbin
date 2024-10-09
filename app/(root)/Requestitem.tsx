ed805ebadf41627c279228b70cf48ef81d2fd093
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-10-09 13:56:13 +0530

    Update Requestitem.tsx

--- app/(root)/Requestitem.tsx
+++ app/(root)/Requestitem.tsx
@@ -1,19 +1,22 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, ScrollView } from 'react-native';
-import { TailwindProvider } from 'tailwindcss-react-native';
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import { useNavigation } from '@react-navigation/native';
-import { router } from 'expo-router';
-import { AntDesign } from '@expo/vector-icons';
-import tw from 'twrnc'; // Tailwind CSS for React Native
-
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView
+} from "react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { useNavigation } from "@react-navigation/native";
+import { router } from "expo-router";
+import { AntDesign } from "@expo/vector-icons";
+import tw from "twrnc"; // Tailwind CSS for React Native
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Requestitem = () => {
-
-
   const [quantity, setQuantity] = useState(5);
   const navigation = useNavigation(); // Hook for navigation inside the component
-
 
   const pricePerKg = 250;
   const totalSellPrice = quantity * pricePerKg;
@@ -31,94 +34,129 @@ const Requestitem = () => {
   return (
     <TailwindProvider>
       <View className="flex-1 bg-gray-100">
-        <View style={{ backgroundColor: '#0C6C41', padding: 16, marginTop: 24 }}>
-          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <View
+          style={{ backgroundColor: "#0C6C41", padding: 16, marginTop: 24 }}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
             <TouchableOpacity onPress={() => router.back()}>
               <AntDesign name="arrowleft" size={24} color="white" />
             </TouchableOpacity>
-            <Text style={{ fontSize: 24, fontWeight: '700', color: 'white', marginLeft: 16 }}>Requesr Items</Text>
+            <Text
+              style={{
+                fontSize: 24,
+                fontWeight: "700",
+                color: "white",
+                marginLeft: 16
+              }}
+            >
+              Requesr Items
+            </Text>
           </View>
         </View>
 
         <ScrollView showsVerticalScrollIndicator={false}>
           {/* Product Card */}
-          <View className="bg-green-100 rounded-lg p-5 ml-4 mr-4 mt-4 mb-5">
-  <View className="flex-row justify-between p-5">
-    {/* Left Section: Product Info */}
-    <View className="items-start mt-12">
-      <FontAwesome5 name="bottle-plastic" size={50} color="black" />
-      <Text className="text-lg mt-2">Plastic</Text>
-    </View>
+          <View className="p-5 mt-4 mb-5 ml-4 mr-4 bg-green-100 rounded-lg">
+            <View className="flex-row justify-between p-5">
+              {/* Left Section: Product Info */}
+              <View className="items-start mt-12">
+                <MaterialCommunityIcons
+                  name="bottle-soda"
+                  size={50}
+                  color="black"
+                />
+                <Text className="mt-2 text-lg">Plastic</Text>
+              </View>
 
-    {/* Right Section: Price and Quantity */}
-    <View className="flex-col items-end ">
-      <View className=" mb-4">
-        <Text className="text-sm text-gray-700 mb-2">Sell Price</Text>
-        <Text className="text-sm text-gray-700">1 kg - LKR 250.00</Text>
-      </View>
-      <Text className="text-lg ml-4 mb-4 mr-2">Quantity (kg)</Text>
-      <View className="flex-row items-center mr-1">
-        <TouchableOpacity onPress={handleDecrement} className="p-1 bg-green-500 rounded w-6">
-          <Text className="text-lg items-center ml-1">-</Text>
-        </TouchableOpacity>
-        <Text className="text-lg mx-5">{quantity}</Text>
-        <TouchableOpacity onPress={handleIncrement} className="p-1 bg-green-500 rounded w-6">
-          <Text className="text-lg ml-1">+</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  </View>
+              {/* Right Section: Price and Quantity */}
+              <View className="flex-col items-end ">
+                <View className="mb-4 ">
+                  <Text className="mb-2 text-sm text-gray-700">Sell Price</Text>
+                  <Text className="text-sm text-gray-700">
+                    1 kg - LKR 250.00
+                  </Text>
+                </View>
+                <Text className="mb-4 ml-4 mr-2 text-lg">Quantity (kg)</Text>
+                <View className="flex-row items-center mr-1">
+                  <TouchableOpacity
+                    onPress={handleDecrement}
+                    className="w-6 p-1 bg-green-500 rounded"
+                  >
+                    <Text className="items-center ml-1 text-lg">-</Text>
+                  </TouchableOpacity>
+                  <Text className="mx-5 text-lg">{quantity}</Text>
+                  <TouchableOpacity
+                    onPress={handleIncrement}
+                    className="w-6 p-1 bg-green-500 rounded"
+                  >
+                    <Text className="ml-1 text-lg">+</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
 
-  {/* Total Price Section */}
-  <View className="ml-48">
-    <Text className="text-xl font-bold text-gray-900">Total Price</Text>
-    <Text className="text-xl font-bold text-red-500">LKR {totalSellPrice.toFixed(2)}</Text>
-  </View>
-</View>
-
+            {/* Total Price Section */}
+            <View className="ml-48">
+              <Text className="text-xl font-bold text-gray-900">
+                Total Price
+              </Text>
+              <Text className="text-xl font-bold text-red-500">
+                LKR {totalSellPrice.toFixed(2)}
+              </Text>
+            </View>
+          </View>
 
           {/* Stepper */}
-          <View style={tw`flex-row justify-between items-center mt-4 p-4`}>
-  {[1, 2, 3].map((step) => (
-    <View key={step} style={tw`flex-row items-center`}>
-      <View
-        style={tw`w-8 h-8 rounded-full border-2 ${
-          step === 1 ? 'bg-green-500 border-green-500' : ' border-green-300'
-        } flex items-center justify-center`}
-      >
-        <Text style={tw`${step === 1 ? 'text-white' : 'text-black'} font-bold`}>
-          {String(step).padStart(2, '0')}
-        </Text>
-      </View>
-      {step < 3 && <View style={tw`h-0.5 w-28 bg-green-300 ml-2`} />}
-    </View>
-  ))}
-</View>
+          <View style={tw`flex-row items-center justify-between p-4 mt-4`}>
+            {[1, 2, 3].map((step) => (
+              <View key={step} style={tw`flex-row items-center`}>
+                <View
+                  style={tw`w-8 h-8 rounded-full border-2 ${
+                    step === 1
+                      ? "bg-green-500 border-green-500"
+                      : " border-green-300"
+                  } flex items-center justify-center`}
+                >
+                  <Text
+                    style={tw`${step === 1 ? "text-white" : "text-black"} font-bold`}
+                  >
+                    {String(step).padStart(2, "0")}
+                  </Text>
+                </View>
+                {step < 3 && <View style={tw`h-0.5 w-28 bg-green-300 ml-2`} />}
+              </View>
+            ))}
+          </View>
 
           {/* Request Details Form */}
-          <View className="mb-5 p-4">
-            <Text className="text-lg font-semibold mb-2">Factory Name</Text>
+          <View className="p-4 mb-5">
+            <Text className="mb-2 text-lg font-semibold">Factory Name</Text>
             <TextInput
               value="Cleantech (Pvt) Ltd"
               editable={false}
-              className="border border-gray-300 p-3 rounded-md mb-3 bg-gray-100"
+              className="p-3 mb-3 bg-gray-100 border border-gray-300 rounded-md"
             />
-            <Text className="text-lg font-semibold mb-2">Factory Address</Text>
+            <Text className="mb-2 text-lg font-semibold">Factory Address</Text>
             <TextInput
               placeholder="Enter factory address"
-              className="border border-gray-300 p-3 rounded-md mb-3"
+              className="p-3 mb-3 border border-gray-300 rounded-md"
             />
-            <Text className="text-lg font-semibold mb-2">Garbage Special type</Text>
+            <Text className="mb-2 text-lg font-semibold">
+              Garbage Special type
+            </Text>
             <TextInput
               value="Plastic"
               editable={false}
-              className="border border-gray-300 p-3 rounded-md bg-gray-100"
+              className="p-3 bg-gray-100 border border-gray-300 rounded-md"
             />
           </View>
 
           {/* Next Button */}
-          <TouchableOpacity className="bg-black p-4 ml-4 mr-4 rounded-lg mb-4 "  onPress={() => navigation.navigate('RequestedItemPayment')}>
-            <Text className="text-white text-center text-lg">NEXT</Text>
+          <TouchableOpacity
+            className="p-4 mb-4 ml-4 mr-4 bg-black rounded-lg "
+            onPress={() => navigation.navigate("RequestedItemPayment")}
+          >
+            <Text className="text-lg text-center text-white">NEXT</Text>
           </TouchableOpacity>
         </ScrollView>
       </View>
